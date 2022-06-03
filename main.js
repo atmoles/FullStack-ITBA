61167a21bb47f0be688c2ff3762e4785daba52fc
Agustin Moles <agussmoles@gmail.com>
2022-06-03 18:07:12 -0300

    Iteraction

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 let arrPersona  = [];
 let arrGastos   = [];
+let total = 0;
+let totalPC = 0;
 
 function f(){
-    let nombre = document.getElementById("nombre");
-    alert(nombre);
-    console.log(nombre);
+    const nombre = document.getElementById("nombre").value;
+    const monto = document.getElementById("monto").value;
+    arrPersona.push(nombre);
+    arrGastos.push(parseInt(monto));
+    total += parseInt(monto);
+    totalPC = parseInt(total)/arrGastos.length;
+    
+
+    let demo = document.getElementById("demo");
+    const p = document.createElement("p");
+    p.innerHTML = nombre + ': ' + monto;
+    demo.append(p);
+
+    let demo2 = document.getElementById("demo2");
+    demo2.innerHTML = total;
+    demo2.append(demo2);
+
+    let demo3 = document.getElementById("demo3");
+    demo3.innerHTML = monto;
+    demo3.append(demo3);
 }
 
